d437d6769dd9a11a90b81a979ba4d12fbb10f7bf
林君潔 <linjunjie@linjunjiedeMacBook-Air.local>
2023-09-18 14:56:22 +0800

    useMemo

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import Breadcrumb from "@/components/Breadcrumb";
 import Navigation from "@/components/Navigation";
 import Shopmenu from "@/components/Shopmenu";
 import ExtraAction from "@/components/ExtraAction";
-
+import { useState, useMemo } from "react";
 import { ProductProvider } from "@data/context";
 
 const Home = () => {
   const [cart, setCart] = useState({});
+  const value = useMemo(() => ({ cart, setCart }), [cart]);
+
   return (
     <ProductProvider value={value}>
       <main className="min-h-screen mx-auto max-w-[1920px]">
